2e6d664f13ed94fd1e3b3749a74b9247e26ab5d4
Anton_Bondar <AntonBondar2013@gmail.com>
2018-10-29 17:33:37 +0200

    Add click event for browse button.

--- src/com/hotfixmaker/Controller.java
+++ src/com/hotfixmaker/Controller.java
@@ -1,4 +1,11 @@
 package com.hotfixmaker;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    public void onClickBrowse(ActionEvent event) {
+    }
 }
